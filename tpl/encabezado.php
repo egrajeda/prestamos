0abb049bbc9df086caa70f5f91eac313bf62155e
Eduardo Grajeda <tatofoo@gmail.com>
2009-05-10 17:17:15 -0600

    Agregando las primeras vistas

---
+++ tpl/encabezado.php
@@ -0,0 +1,16 @@
+<html>
+  <head>
+    <title><?php echo $vista->titulo ?></title>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />    
+    <link href="css/estilo.css" rel="stylesheet" type="text/css" />
+    <link href="css/round-button.css" rel="stylesheet" type="text/css" /> 
+    <script type="text/javascript" src="js/jquery.js"></script>          
+    <script type="text/javascript" src="js/efectos.js"></script>
+  </head>
+  <body>
+    <div id="contenedor">   
+      <div id="encabezado">
+        <div id="logo"></div>        
+      </div>
+      <div id="contenido">
+        <h1><?php echo $vista->encabezado ?></h1>
---
+++ tpl/login.php
@@ -0,0 +1,23 @@
+<center>
+  <table class="form" cellpadding="8" cellspacing="0" border="0">
+    <tr>
+      <td width="30%" align="right">Usuario:</td>
+      <td width="70%">
+        <input class="input" type="text" name="usuario" value="" />
+      </td>
+    </tr>
+    <tr>
+      <td align="right">Contraseña:</td>
+      <td><input class="input" type="password" name="password" value="" /></td>
+    </tr>        
+    <tr>
+      <td></td>
+      <td>
+        <a class="button" href="#"><span>Iniciar sesión</span></a> 
+      </td>
+    </tr>
+  </table>
+  <div class="error">
+    <span>La información introducida es incorrecta.</span>
+  </div>        
+</center>
