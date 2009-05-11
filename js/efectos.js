634b0be95bf6997304fb77a60a089e6d2e172e55
Eduardo Grajeda <tatofoo@gmail.com>
2009-05-11 14:07:19 -0600

    Ajustando el calendario

--- js/efectos.js
+++ js/efectos.js
@@ -5,5 +5,18 @@ $(document).ready(function() {
   $('.input').blur(function() {
     $(this).removeClass('input-highlight');
   });
-  $('#date').datepicker();
+  /* Los nombres de los dias y meses en español */
+  var meses = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio',
+               'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'];
+  var dias = ['L', 'M', 'Mi', 'J', 'V', 'S', 'D'];  
+  /* Si hay un calendario en la pagina, dependiendo del dia vamos a habilitar
+   * las fechas en que puede reservar el equipo */    
+  var d = new Date();
+  if (d.getDay() == 5) {
+    $('#date').datepicker({ minDate: '+2d', maxDate: '+8d',
+      dayNamesMin: dias, monthNames: meses });
+  } else if (d.getDay() == 1) {
+    $('#date').datepicker({ minDate: '+0d', maxDate: '+6d',
+      dayNamesMin: dias, monthNames: meses });
+  }  
 });
